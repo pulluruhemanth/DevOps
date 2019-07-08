55d88cc03e5f4241066507d5e8c422866b33c526
Hemanth pulluru Cloud <pulluruhemanth9995@gmail.com>
2019-07-08 17:22:29 +0000

    new commit

--- helloworld.java
+++ helloworld.java
@@ -4,8 +4,6 @@ publicc class HelloWorld {
                 System.out.println("Hello, World");
                 System.out.println("Hello, change2");
                 System.out.println("Hello, change3");
-                System.out.println("Hello, good"); 
-                
                 // change #2
         }
 }
